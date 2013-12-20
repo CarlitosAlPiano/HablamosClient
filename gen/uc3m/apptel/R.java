3d6eaa2b71cdb01a33c79edbe1e2adf99b3749de
Carlos Ruiz <cruiz010@ucr.edu>
2013-12-20 11:27:53 +0100

    Subida parcial 4 (ultima)
    GitHub se ha rayado y tengo que subir los archivos por bloques.

--- gen/uc3m/apptel/R.java
+++ gen/uc3m/apptel/R.java
@@ -19,8 +19,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int AMARILLO=0x7f040001;
-        public static final int black_overlay=0x7f040000;
+        public static final int app_background=0x7f040000;
+        public static final int black=0x7f040002;
+        public static final int black_overlay=0x7f040001;
+        public static final int orange=0x7f040004;
+        public static final int white=0x7f040005;
+        public static final int yellow=0x7f040003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -36,75 +40,78 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f090015;
-        public static final int action_settings=0x7f090016;
-        public static final int btn1=0x7f090009;
-        public static final int email=0x7f090003;
-        public static final int fila1000=0x7f09000c;
-        public static final int fila1001=0x7f09000f;
-        public static final int fila1002=0x7f090011;
-        public static final int fila1003=0x7f090013;
-        public static final int fullscreen_content=0x7f090007;
-        public static final int fullscreen_content_controls=0x7f090008;
-        public static final int listaUsuarios=0x7f09000b;
-        public static final int login=0x7f090005;
-        public static final int login_form=0x7f090002;
-        public static final int login_status=0x7f090000;
-        public static final int login_status_message=0x7f090001;
-        public static final int password=0x7f090004;
-        public static final int sign_in_button=0x7f090006;
-        public static final int txt1000=0x7f09000d;
-        public static final int txt1001=0x7f090010;
-        public static final int txt1002=0x7f090012;
-        public static final int txt1003=0x7f090014;
-        public static final int txtMyID=0x7f09000e;
+        public static final int action_forgot_password=0x7f09001d;
+        public static final int action_settings=0x7f09001c;
+        public static final int btnConnect=0x7f09000b;
+        public static final int btnSendMessage=0x7f09001b;
+        public static final int chatItemImgMessage=0x7f090016;
+        public static final int chatItemTxtMessage=0x7f090017;
+        public static final int email=0x7f090005;
+        public static final int fila1000=0x7f09000d;
+        public static final int fila1001=0x7f090010;
+        public static final int fila1002=0x7f090012;
+        public static final int fila1003=0x7f090014;
+        public static final int layoutNewMessage=0x7f090019;
+        public static final int listaUsuarios=0x7f09000c;
+        public static final int login=0x7f090007;
+        public static final int login_form=0x7f090004;
+        public static final int login_status=0x7f090002;
+        public static final int login_status_message=0x7f090003;
+        public static final int lstConversacion=0x7f090018;
+        public static final int pager=0x7f090000;
+        public static final int pager_title_strip=0x7f090001;
+        public static final int password=0x7f090006;
+        public static final int sign_in_button=0x7f090008;
+        public static final int textView1=0x7f090009;
+        public static final int txt1000=0x7f09000e;
+        public static final int txt1001=0x7f090011;
+        public static final int txt1002=0x7f090013;
+        public static final int txt1003=0x7f090015;
+        public static final int txtMyID=0x7f09000f;
+        public static final int txtNewMessage=0x7f09001a;
         public static final int txtUsuario=0x7f09000a;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_user_list=0x7f030002;
+        public static final int activity_chat=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_user_list=0x7f030003;
+        public static final int chat_list_item=0x7f030004;
+        public static final int fragment_chat=0x7f030005;
     }
     public static final class menu {
-        public static final int login=0x7f080000;
-        public static final int user_list=0x7f080001;
+        public static final int chat=0x7f080000;
+        public static final int login=0x7f080001;
+        public static final int user_list=0x7f080002;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f06000b;
-        public static final int action_settings=0x7f060004;
-        public static final int action_sign_in_register=0x7f060009;
-        public static final int action_sign_in_short=0x7f06000a;
+        public static final int action_forgot_password=0x7f060010;
+        public static final int action_settings=0x7f060002;
+        public static final int action_sign_in_register=0x7f06000e;
+        public static final int action_sign_in_short=0x7f06000f;
         public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060001;
-        public static final int dummy_content=0x7f060002;
-        public static final int error_field_required=0x7f060010;
-        public static final int error_incorrect_password=0x7f06000f;
-        public static final int error_invalid_email=0x7f06000d;
-        public static final int error_invalid_password=0x7f06000e;
-        public static final int hello_world=0x7f060005;
-        public static final int login_progress_signing_in=0x7f06000c;
+        public static final int debugBtnConnect=0x7f060005;
+        public static final int enviar_mensaje=0x7f060004;
+        public static final int error_field_required=0x7f060015;
+        public static final int error_incorrect_password=0x7f060014;
+        public static final int error_invalid_email=0x7f060012;
+        public static final int error_invalid_password=0x7f060013;
+        public static final int hint_user_id=0x7f06000a;
+        public static final int icono_de_hablamos=0x7f060003;
+        public static final int login_progress_signing_in=0x7f060011;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f060007;
-        public static final int prompt_password=0x7f060008;
-        public static final int title_activity_login=0x7f060006;
-        public static final int title_activity_user_list=0x7f060003;
+        public static final int prompt_email=0x7f06000c;
+        public static final int prompt_password=0x7f06000d;
+        public static final int title_activity_chat=0x7f060006;
+        public static final int title_activity_login=0x7f06000b;
+        public static final int title_activity_user_list=0x7f060001;
+        public static final int title_section1=0x7f060007;
+        public static final int title_section2=0x7f060008;
+        public static final int title_section3=0x7f060009;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-
-
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-
-
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
 
@@ -117,16 +124,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f070001;
-        public static final int ButtonBar=0x7f070003;
-        public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070006;
-        public static final int FullscreenTheme=0x7f070002;
-        public static final int LoginFormContainer=0x7f070005;
+        public static final int AppBaseTheme=0x7f070003;
+        public static final int AppTheme=0x7f070000;
+        public static final int Boton=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
     public static final class styleable {
         /** 
